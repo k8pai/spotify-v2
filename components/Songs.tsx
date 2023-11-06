0899b31ab7833f86ddea4ff5a8ea899dc20e9eac
k8pai <thek8pai@gmail.com>
2023-11-06 23:50:05 +0530

    Ui Update

--- components/Songs.tsx
+++ components/Songs.tsx
@@ -5,7 +5,7 @@ import { Playlist, PlaylistData } from '../typings';
 import SpotifyWebApi from 'spotify-web-api-node';
 import Image from 'next/image';
 import { convertMsToHMS } from '../lib/helpers';
-import { Table } from '@radix-ui/themes';
+import { Box, Card, Flex, Grid, Table, Text } from '@radix-ui/themes';
 
 const Song = ({
 	track,
@@ -15,32 +15,59 @@ const Song = ({
 	order: number;
 }) => {
 	return (
-		<Table.Row style={{ color: 'whitesmoke' }}>
-			<Table.Cell>{order}</Table.Cell>
-			<Table.Cell>
-				<div className="flex overflow-hidden flex-nowrap items-center space-x-2">
-					<Image
-						src={track?.album.images?.[0].url ?? ''}
-						alt={track?.name ?? ''}
-						height={40}
-						width={40}
-						className="rounded-md shadow-sm"
-					/>
-					<div>
-						<div>{track?.name}</div>
-						<div>
-							{track?.artists
-								.map((artist) => artist.name)
-								.join(', ')}
-						</div>
+		<Grid columns="2" gap="6">
+			<Box className="p-2 w-full box-border truncate">
+				<Flex className="items-center justify-around">
+					<Text as="div" size="2" className="w-6">
+						{order}
+					</Text>
+					<div className="box-border ">
+						<Flex gap="3" align="center">
+							<Image
+								src={track?.album.images?.[0]?.url ?? ''}
+								alt={track?.name ?? ''}
+								height={40}
+								width={40}
+								className="rounded-md shadow-sm"
+							/>
+							<Box>
+								<Text
+									as="div"
+									size="2"
+									weight="bold"
+									className="text-white w-full truncate"
+								>
+									{track?.name}
+								</Text>
+								<Text
+									as="div"
+									size="2"
+									className="text-inherit"
+								>
+									{track?.artists
+										.map((artist) => artist.name)
+										.join(', ')}
+								</Text>
+							</Box>
+						</Flex>
 					</div>
-				</div>
-			</Table.Cell>
-			<Table.Cell className="hidden xl:block">
-				<div>{track?.album.name}</div>
-			</Table.Cell>
-			<Table.Cell>{convertMsToHMS(track?.duration_ms!)}</Table.Cell>
-		</Table.Row>
+				</Flex>
+			</Box>
+			<Box className="p-2">
+				<Flex
+					align={'center'}
+					justify={'between'}
+					className="items-center lg:justify-between justify-between"
+				>
+					<div className=" ">
+						<div>{track?.album.name}</div>
+					</div>
+					<div className="">
+						{convertMsToHMS(track?.duration_ms!)}
+					</div>
+				</Flex>
+			</Box>
+		</Grid>
 	);
 };
 
@@ -48,36 +75,17 @@ const Songs = () => {
 	const playlist = useRecoilValue<PlaylistData | null>(playlistState);
 
 	console.log('playlist => ', playlist);
-
 	return (
-		<div className="text-white px-8 flex flex-col space-y-2 pb-28">
-			<Table.Root variant="ghost" style={{ color: 'whitesmoke' }}>
-				<Table.Header color="#fff">
-					<Table.Row>
-						<Table.ColumnHeaderCell className="text-zinc-400">
-							#
-						</Table.ColumnHeaderCell>
-						<Table.ColumnHeaderCell className="text-zinc-400">
-							Title
-						</Table.ColumnHeaderCell>
-						<Table.ColumnHeaderCell className="text-zinc-400 hidden xl:block">
-							Album
-						</Table.ColumnHeaderCell>
-						<Table.ColumnHeaderCell className="text-zinc-400">
-							Duration
-						</Table.ColumnHeaderCell>
-					</Table.Row>
-				</Table.Header>
-				{playlist?.tracks.items.map((item, _) => {
-					return (
-						<Song
-							key={item?.track?.id}
-							track={item?.track}
-							order={_ + 1}
-						/>
-					);
-				})}
-			</Table.Root>
+		<div className="px-8 text-zinc-500">
+			{playlist?.tracks.items.map((item, _) => {
+				return (
+					<Song
+						key={item?.track?.id}
+						track={item?.track}
+						order={_ + 1}
+					/>
+				);
+			})}
 		</div>
 	);
 };
